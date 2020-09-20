fe192eaeb9949db5326ab1e73f82e9b2965592af
Vinicius Bueno <virods@gmail.com>
2020-09-20 17:06:42 -0300

    color palette

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,11 @@
 
 export const PAGE_NAME = 'Cursos disponíveis'
+export const COLLOR_PALLETE = {
+  alloy_orange: 'C45E1A',
+  carrot_orange: 'F29C29',
+  mellow_apricot: 'F3BE83',
+  usafa_blue: '0556AD',
+  french_sky_blue: '7FBAFB',
+  spannish_gray: '9D9999',
+  cultured: 'F5F5F7',
+}
