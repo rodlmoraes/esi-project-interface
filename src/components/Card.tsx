b313cabfe5e627a2d253cd69791671c10c4aac4b
João Francisco Cocca Fukuda <joao.fukuda@protonmail.com>
2020-09-27 20:25:26 +0000

    feat(list): adicionar conexão com o back-end
    Adicionar chamadas ao back-end do mesmo jeito que o TeacherForm para
    poder listar as aulas que estão no servidor postgres

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { makeStyles, createStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
