0cea9c1d866634a27c94584dfcac075ffe2de628
rogerzs <56163805+rogerzs@users.noreply.github.com>
2020-10-12 10:08:54 -0300

    Adicionando Detalhes de Aulas aos Usuários
    Uma das funções exigidas pelo cliente é que o usuário pudesse clicar em um botão e obter os detalhes da aula que irá ou não assistir.
    
    O Botão foi acrescentado e funciona perfeitamente

--- src/components/LessonCard.tsx
+++ src/components/LessonCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { makeStyles, createStyles } from '@material-ui/core/styles'
 import Card from '@material-ui/core/Card'
 import CardHeader from '@material-ui/core/CardHeader'
@@ -9,6 +9,7 @@ import Typography from '@material-ui/core/Typography'
 import { red } from '@material-ui/core/colors'
 import TeacherImg from '../assets/images/9k.png'
 import Button from '@material-ui/core/Button'
+import Modal from '@material-ui/core/Modal'
 
 type LessonCardProps = {
   name: string
@@ -43,7 +44,7 @@ const useStyles = makeStyles(() =>
 
 export default function LessonCard({ name, description, link }: LessonCardProps) {
   const classes = useStyles()
-
+  const [modalIsOpen, setModalIsOpen] = useState(false)
   return (
     <Card className={classes.root}>
       <CardHeader
@@ -62,8 +63,17 @@ export default function LessonCard({ name, description, link }: LessonCardProps)
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <Button className={classes.button} variant='contained' color='primary' >Ver aula</Button>
+        <Button onClick={() => setModalIsOpen(true)} className={classes.button} variant='contained' color='primary' >Detalhes</Button>
       </CardActions>
+      <Modal
+        open={modalIsOpen}
+      >
+        <div>  
+          <p> Tema da Aula: {name} </p>
+          <p> Descrição: {description} </p>
+          <p> Link direto para aula: {link} </p>
+        </div>
+      </Modal> 
     </Card>
   )
 }
