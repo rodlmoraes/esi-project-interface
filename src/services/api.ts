6e9d3510d2e20be75b259842c3ee87c42df234f6
rodlmoraes <rodlmoraes@hotmail.com>
2020-10-12 12:03:29 -0300

    remove empty space

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL =  process.env.NODE_ENV === 'development'
+const baseURL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:8000/api'
   : 'https://salutis.herokuapp.com/api'
 
