e3c18cf32a2bbf0e27f497dad31a8d1bc4f809df
Júlia Passos <juliapassos@usp.br>
2020-10-12 10:59:57 -0300

    fixed header test

--- src/__tests__/pages/TeacherForm.spec.tsx
+++ src/__tests__/pages/TeacherForm.spec.tsx
@@ -7,7 +7,6 @@ test('renders LessonForm', () => {
   const { getByText, getAllByText } = render(<LessonForm />)
   getByText('Cadastro de Aula')
   getAllByText('Nome da aula')
-  getAllByText('Nome do Professor')
   getAllByText('Link da Aula')
   getAllByText('Descrição da Aula')
   getByText('Salvar Cadastro')
